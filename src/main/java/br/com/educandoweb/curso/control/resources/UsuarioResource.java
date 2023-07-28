36feef737a8e133dcdb9fbd3507fb94a70e8c810
Marcos Pereira da Silva Neto <marcosp.neto@unifacol.edu.br>
2023-07-28 11:08:33 -0300

    Usuario insert

--- src/main/java/br/com/educandoweb/curso/control/resources/UsuarioResource.java
+++ src/main/java/br/com/educandoweb/curso/control/resources/UsuarioResource.java
@@ -1,13 +1,17 @@
 package br.com.educandoweb.curso.control.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.educandoweb.curso.model.entidades.Usuario;
 import br.com.educandoweb.curso.model.servicos.UsuarioServico;
@@ -27,5 +31,13 @@ public class UsuarioResource {
 	public ResponseEntity<Usuario> findById(@PathVariable Long id){
 		Usuario obj = servico.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj){
+		obj = servico.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- src/main/java/br/com/educandoweb/curso/model/servicos/UsuarioServico.java
+++ src/main/java/br/com/educandoweb/curso/model/servicos/UsuarioServico.java
@@ -22,5 +22,9 @@ public class UsuarioServico {
 		Optional<Usuario> obj = repositorio.findById(id);
 		return obj.get();
 	}
+	
+	public Usuario insert (Usuario obj) {
+		return repositorio.save(obj);
+	}
 
 }
